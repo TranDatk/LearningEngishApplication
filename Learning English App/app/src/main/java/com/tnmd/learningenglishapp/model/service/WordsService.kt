92d4b1d8dfba1eddb5704720e3d4c65162c1c56e
TranDatk <hgooshvhd123@gmail.com>
2023-09-22 22:30:31 +0700

    update

--- Learning English App/app/src/main/java/com/tnmd/learningenglishapp/model/service/WordsService.kt
+++ Learning English App/app/src/main/java/com/tnmd/learningenglishapp/model/service/WordsService.kt
@@ -6,6 +6,6 @@ import kotlinx.coroutines.flow.Flow
 interface WordsService {
 
     val words: Flow<List<Words>>
-
     suspend fun getWords(wordsId: String): Words?
+    suspend fun findAWordsWithName(wordsName : String) : Words?
 }
--- Learning English App/app/src/main/java/com/tnmd/learningenglishapp/model/service/imple/WordsServiceImpl.kt
+++ Learning English App/app/src/main/java/com/tnmd/learningenglishapp/model/service/imple/WordsServiceImpl.kt
@@ -6,6 +6,7 @@ import com.google.firebase.firestore.ktx.toObject
 import com.tnmd.learningenglishapp.model.Words
 import com.tnmd.learningenglishapp.model.service.WordsService
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
@@ -19,7 +20,22 @@ class WordsServiceImpl @Inject constructor(private val firestore: FirebaseFirest
     override suspend fun getWords(wordsId: String): Words? =
         firestore.collection(WORDS_COLLECTION).document(wordsId).get().await().toObject()
 
+    override suspend fun findAWordsWithName(wordsName: String): Words? {
+        val querySnapshot = firestore.collection(WORDS_COLLECTION)
+            .whereEqualTo(WORDS_NAME_FIELD, wordsName)
+            .get()
+            .await()
+        if (!querySnapshot.isEmpty) {
+            // Lấy ra đối tượng đầu tiên từ kết quả truy vấn
+            val document = querySnapshot.documents[0]
+            // Chuyển đổi dữ liệu từ DocumentSnapshot thành đối tượng Words
+            return document.toObject(Words::class.java)
+        }
+        return null
+    }
+
     companion object {
         private const val WORDS_COLLECTION = "courses"
+        private const val WORDS_NAME_FIELD = "name"
     }
 }
